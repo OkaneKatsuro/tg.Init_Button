694104aa091fd0e55663c13608284145d1ec9aa0
ROMAN MAKSIMOV <104910769+OkaneKatsuro@users.noreply.github.com>
2024-02-12 19:08:28 +0100

    validation

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import { tg } from "./tg.api";
 import "./App.css";
 
 const App = () => {
-  const [initData, setInitData] = useState(null);
+  interface User {
+    id: number;
+    first_name: string;
+    last_name: string;
+    username: string;
+    language_code: string;
+    allows_write_to_pm: boolean;
+  }
+
+  const [userData, setUserData] = useState<User | null>(null);
 
   useEffect(() => {
     tg.ready();
@@ -11,16 +20,31 @@ const App = () => {
   }, []);
 
   const handleButtonClick = () => {
-    const data = tg.initData;
-    setInitData(data);
+    const initData = tg.initData;
+
+    const params = new URLSearchParams(initData);
+
+    const userJson = params.get("user");
+    if (userJson) {
+      const user = JSON.parse(decodeURIComponent(userJson));
+      setUserData(user);
+    }
   };
 
   return (
     <div>
       <h1>init.data:</h1>
-      <div className="data-container">{initData}</div>
-
-      <p></p>
+      <div className="data-container">
+        {userData ? (
+          <div>
+            <p>Username: {userData.username}</p>
+            <p>First Name: {userData.first_name}</p>
+            <p>Last Name: {userData.last_name}</p>
+          </div>
+        ) : (
+          <p>No user data</p>
+        )}
+      </div>
       <button className="button" onClick={handleButtonClick}>
         INIT.Data
       </button>
