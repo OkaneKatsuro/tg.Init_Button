b84d472b5a8e29acba74d2de53e7e002b3b5e897
ROMAN MAKSIMOV <104910769+OkaneKatsuro@users.noreply.github.com>
2024-02-12 19:26:19 +0100

    ffffff

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
   const handleButtonClick = () => {
     const params = new URLSearchParams(initData);
 
-    const userJson = params.get("user");
+    const userJson = params.get("query_id");
     if (userJson) {
       const user = JSON.parse(decodeURIComponent(userJson));
       setUserData(user);
